10f42e57e02de5f3d82cb861a61afe26471f3083
fcaussin <fabien.caussin@icloud.com>
2018-05-17 00:43:08 +0200

    Création des fichiers de configuration permettant la  connexion à la BD

---
+++ app/config/Configuration.php
@@ -0,0 +1,39 @@
+<?php
+  namespace App\Config;
+
+  /**
+   * Configuration des paramètres de connexion à la base de données
+   */
+  class Configuration
+  {
+    private static $parameters;
+
+
+    // Renvoie la valeur d'un paramètre de configuration
+    public static function get($name, $defaultValue = null)
+    {
+      if (isset(self::getParameters()[$name])) {
+        $value = self::getParameters()[$name];
+      }
+      else {
+        $value = $defaultValue;
+      }
+      return $value;
+    }
+
+    // Renvoie le tableau des paramètres en le chargeant au besoin
+    private static function getParameters()
+    {
+      if (self::$parameters == null) {
+        $file = "../app/config/config.ini";
+        if (!file_exists($file)) {
+          throw new \Exception("Auncun fichier de configuration trouvé");
+        }
+        else {
+          self::$parameters = parse_ini_file($file);
+        }
+      }
+      return self::$parameters;
+    }
+  }
+?>
--- app/models/ArticlesManager.php
+++ app/models/ArticlesManager.php
@@ -1,7 +1,7 @@
 <?php
   namespace App\Models;
 
-  use App\Models\PDOManager;
+  use App\Config\PDOManager;
 
 
   class ArticlesManager extends PDOManager
